03fbc8e7d97228a8107da54d8f1a4004bf9deb60
Jahnvi <jpate201@uic.edu>
2017-12-07 10:13:46 -0600

    Added MenuBar to LoginFrame

--- src/LoginFrame.java
+++ src/LoginFrame.java
@@ -1,17 +1,22 @@
-
-
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.KeyStroke;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import java.awt.event.KeyAdapter;
 
 public class LoginFrame extends JFrame {
 	private static final long serialVersionUID = 1L;
@@ -24,6 +29,9 @@ public class LoginFrame extends JFrame {
 	private JLabel lblPort;
 	private JLabel lblAddressDesc;
 	private JLabel lblPortDesc;
+	private JMenu about; 
+	private JMenuItem aboutAuthors;
+	
 
 	public LoginFrame() {
 		try {
@@ -34,7 +42,7 @@ public class LoginFrame extends JFrame {
 		setResizable(false);
 		setTitle("Login");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(300, 380);
+		setSize(450, 480);
 		setLocationRelativeTo(null);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -42,41 +50,42 @@ public class LoginFrame extends JFrame {
 		contentPane.setLayout(null);
 
 		txtName = new JTextField();
-		txtName.setBounds(67, 50, 165, 28);
+		txtName.setBounds(142, 50, 165, 28);
 		contentPane.add(txtName);
 		txtName.setColumns(10);
 
 		JLabel lblName = new JLabel("Name:");
-		lblName.setBounds(127, 34, 45, 16);
+		lblName.setBounds(202, 34, 45, 16);
 		contentPane.add(lblName);
 
 		txtAddress = new JTextField();
-		txtAddress.setBounds(67, 116, 165, 28);
+		txtAddress.setBounds(142, 116, 165, 28);
 		contentPane.add(txtAddress);
 		txtAddress.setColumns(10);
 
 		lblIpAddress = new JLabel("IP Address:");
-		lblIpAddress.setBounds(111, 96, 77, 16);
+		lblIpAddress.setBounds(186, 96, 77, 16);
 		contentPane.add(lblIpAddress);
 
 		txtPort = new JTextField();
 		txtPort.setColumns(10);
-		txtPort.setBounds(67, 191, 165, 28);
+		txtPort.setBounds(142, 191, 165, 28);
 		contentPane.add(txtPort);
 
 		lblPort = new JLabel("Port:");
-		lblPort.setBounds(133, 171, 34, 16);
+		lblPort.setBounds(208, 171, 34, 16);
 		contentPane.add(lblPort);
 
 		lblAddressDesc = new JLabel("(eg. 192.168.0.2)");
-		lblAddressDesc.setBounds(94, 142, 112, 16);
+		lblAddressDesc.setBounds(169, 142, 112, 16);
 		contentPane.add(lblAddressDesc);
 
-		lblPortDesc = new JLabel("(eg. 8192)");
-		lblPortDesc.setBounds(116, 218, 68, 16);
+		lblPortDesc = new JLabel("(eg. 1013)");
+		lblPortDesc.setBounds(191, 218, 68, 16);
 		contentPane.add(lblPortDesc);
 
 		JButton btnLogin = new JButton("Login");
+		
 		btnLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String name = txtName.getText();
@@ -84,9 +93,57 @@ public class LoginFrame extends JFrame {
 				int port = Integer.parseInt(txtPort.getText());
 				login(name, address, port);
 			}
+			
+			
 		});
-		btnLogin.setBounds(91, 311, 117, 29);
+		btnLogin.setBounds(125, 408, 200, 35);
 		contentPane.add(btnLogin);
+		
+		/**
+		 * Add a MenuBar for the the Login Window
+		 * 	with a About, Help and Exit 
+		 */
+		JMenuBar loginMenuBar = new JMenuBar();
+		loginMenuBar.setBounds(0, 0, 474, 22);
+		contentPane.add(loginMenuBar);
+
+		about = new JMenu("About");
+		loginMenuBar.add(about);
+		
+		aboutAuthors = new JMenuItem("About Authors");
+		aboutAuthors.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, ActionEvent.CTRL_MASK));
+		about.add(aboutAuthors);
+		aboutAuthors.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "Authors of Networked Chat:\nJahnvi Patel (jpate201)\nPatrick O'Connell (oconne16)\nDeep Mehta (dmehta22)");
+			}
+		});
+		
+		JMenu help = new JMenu("Help");
+		loginMenuBar.add(help);
+		
+		/*Provide help with connection*/
+		JMenuItem helpNetworkedChat = new JMenuItem("How to Connect");
+		help.add(helpNetworkedChat);
+		helpNetworkedChat.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "How to Connect:");
+			}
+		});
+		
+		
+		JMenu exit = new JMenu("Exit");
+		loginMenuBar.add(exit);
+		JMenuItem quitGame = new JMenuItem("Quit");
+		quitGame.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, ActionEvent.CTRL_MASK));
+		exit.add(quitGame);
+		quitGame.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		
+		
 	}
 
 	private void login(String name, String address, int port) {
